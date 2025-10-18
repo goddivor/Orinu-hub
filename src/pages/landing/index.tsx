a71d767626226690a8d9e1527d89a49f3959fd78
Divor <todjropatrice2002@gmail.com>
2025-10-18 18:06:27 +0200

    Initalisation du projet avec vite + react + jwc

---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Landing: React.FC = () => {
+    return (
+        <main>
+           <div>salut voici la page principal de notre sit web</div>
+        </main>
+    );
+};
+
+export default Landing;
